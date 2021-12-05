cf60d5ee523de31ff01508b9daa2bd4fae14670f
dstoyanov <dobromir.nikolov.s@gmail.com>
2021-12-05 18:21:45 +0200

    Reversing testing changes

--- dom-introduction/01.Edit Element/EditElement.js
+++ dom-introduction/01.Edit Element/EditElement.js
@@ -2,5 +2,5 @@ function editElement(ref, match, replacer) {
     const content = ref.textContent;
     const matcher = new RegExp(match, 'g');
     const edited = content.replace(matcher, replacer);
-    // ref.textContent = edited;
+    ref.textContent = edited;
   }
